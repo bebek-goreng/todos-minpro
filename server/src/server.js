e413e189f4e9bce4c09433d81d5dc38090adf9e3
mahen <oresuki01@gmail.com>
2024-09-17 13:15:08 +0700

    setup server

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+export const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(morgan('dev'));
+
