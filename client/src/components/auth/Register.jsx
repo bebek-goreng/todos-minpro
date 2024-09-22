24d04e0d8bc16c86b052d7bddc300a8955291620
mahen <oresuki01@gmail.com>
2024-09-22 10:37:06 +0700

    add: feat auth register

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -3,22 +3,47 @@
 import { useState } from "react";
 import { Input } from "../Input";
 import { Button } from "../Button";
-import { toast, ToastContainer } from "react-toastify";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import { register } from "@/modules/fetch-user";
+import { useRouter } from "next/navigation";
 
 export const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
 
-  const handleSubmit = (e) => {
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword((prevState) => !prevState);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (password !== confirmPassword) {
-      console.log("Passwords do not match");
+      setErrorMessage("Passwords do not match");
       return;
     }
-    console.log("Register dengan:", { email, password });
-    toast.success("test");
+    setErrorMessage("");
+
+    try {
+      const data = { name, email, password };
+      await register(data);
+
+      alert("Register Success");
+      router.push("/");
+    } catch (error) {
+      console.log(error.message);
+      alert("register failed");
+    }
   };
 
   return (
@@ -42,28 +67,56 @@ export const Register = () => {
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter your email"
       />
-      <Input
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Enter your password"
-      />
-      <Input
-        label="Confirm Password"
-        type="password"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-        placeholder="Confirm your password"
-      />
-      <Button text="Register" type="submit" className={"mt-2"} />
+      <div className="relative">
+        <Input
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute right-3 top-9 text-gray-600">
+          {showPassword ? (
+            <EyeSlashIcon className="h-5 w-5" />
+          ) : (
+            <EyeIcon className="h-5 w-5" />
+          )}
+        </button>
+      </div>
+      <div className="relative">
+        <Input
+          label="Confirm Password"
+          type={showConfirmPassword ? "text" : "password"}
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          placeholder="Confirm your password"
+        />
+        <button
+          type="button"
+          onClick={toggleConfirmPasswordVisibility}
+          className="absolute right-3 top-9 text-gray-600">
+          {showConfirmPassword ? (
+            <EyeSlashIcon className="h-5 w-5" />
+          ) : (
+            <EyeIcon className="h-5 w-5" />
+          )}
+        </button>
+      </div>
+      {errorMessage && (
+        <p className="text-red-500 text-sm mt-1 text-center mb-2">
+          {errorMessage}
+        </p>
+      )}
+      <Button text="Register" type="submit" className="mt-2" />
       <p className="text-black text-sm text-center mt-3">
         Already have an account?
         <a href="/auth/login" className="text-blue-600 ml-1">
           Login
         </a>
       </p>
-      <ToastContainer />
     </form>
   );
 };
