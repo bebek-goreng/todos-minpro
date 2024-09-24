41262abec96b5b94e7e920acc10bb3c1804ae77b
mahen <oresuki01@gmail.com>
2024-09-24 13:27:44 +0700

    add: feature todos my day

--- client/src/app/my-day/page.jsx
+++ client/src/app/my-day/page.jsx
@@ -1,11 +1,38 @@
+"use client";
+
 import Layout from "@/components/view/Layout";
 import TaskList from "@/components/view/TaskList";
+import { getAllTodos } from "@/modules/fetch-todos";
+import { useEffect, useState } from "react";
 
 const MydDay = () => {
+  const [task, setTask] = useState([]);
+
+  const fetchTask = async () => {
+    try {
+      const response = await getAllTodos({
+        category: "MyDay",
+        status: "Incomplete",
+      });
+
+      setTask(response.data);
+    } catch (error) {
+      console.log(`error fetching todos data: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchTask();
+  }, []);
+
+  const taskMyDay = task.filter((t) => {
+    return t.category === "MyDay" && t.status === "incomplete";
+  });
+
   return (
     <div>
       <Layout>
-        <TaskList />
+        <TaskList tasks={taskMyDay} fetchTask={fetchTask} />
       </Layout>
     </div>
   );
